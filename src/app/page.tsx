5bed22fbea7206704972de8836c5f85f383c8fdd
nilav-talukdar <nilavtalukdar49@gmail.com>
2025-08-18 20:24:55 +0530

    modified some important component according to the design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <Button variant="destructive">Click Me</Button>
+      <Button variant="elevated">I am a button</Button>
       <p className="text-rose-500">Inklore</p>
+      <Input
+        placeholder="Enter your name"
+        type="text"
+        className="my-2 max-w-md"
+      />
+      <Progress value={50} className="my-2" />
+      <Textarea className="my-2 max-w-md" placeholder="Enter your bio" />
     </div>
   );
 }
